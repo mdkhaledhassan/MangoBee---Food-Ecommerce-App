2bfdac4e0c13990f9cc03d7e09a5ba99b5be5134
mdkhaledhassan <khaledbubt@gmail.com>
2022-09-18 23:57:44 +0600

    Khaled

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false, home: DashboardScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
